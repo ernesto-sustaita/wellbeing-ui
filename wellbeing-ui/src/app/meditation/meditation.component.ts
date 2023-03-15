0fc123a53b5dd71f4f978fbd5baba6ac14c136da
Ernesto Sustaita <ernesto.sustaita@yahoo.fr>
2023-03-14 23:44:03 -0600

    Add distraction button

--- wellbeing-ui/src/app/meditation/meditation.component.ts
+++ wellbeing-ui/src/app/meditation/meditation.component.ts
@@ -9,12 +9,16 @@ export class MeditationComponent implements OnInit {
 
   constructor() { }
 
+  distractions = 0;
+  isDistractedButtonVisible = false;
+
   ngOnInit(): void {
   }
 
   playBell() {
     const audio = new Audio('assets/sound/tibetan-bell-sound-8s.mp3');
     audio.play();
+    this.isDistractedButtonVisible = true;
   }
 
 }
