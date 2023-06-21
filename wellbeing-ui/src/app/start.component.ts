5fa8e6cd16492b8b0e9f1735576ebe730a85c533
ernesto-sustaita <50428180+ernesto-sustaita@users.noreply.github.com>
2023-06-20 19:01:38 -0600

    Calendar draft

--- wellbeing-ui/src/app/start.component.ts
+++ wellbeing-ui/src/app/start.component.ts
@@ -5,20 +5,26 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class StartComponent implements OnInit {
-    
+
     ngOnInit(): void {
-        let month = this.dateObject.getMonth();
-        let year = this.dateObject.getFullYear();
-        this.currentMonth = this.monthNames[month];
-        this.daysInMonth = this.getDaysInMonth(year,month);
-        this.monthStartsWith = this.getDayMonthStartsWith(year,month);
+      // Months start in 0 = January
+      let month = this.dateObject.getMonth();
+      let year = this.dateObject.getFullYear();
+      this.currentMonth = this.monthNames[month];
+      this.daysInMonth = this.getDaysInMonth(year,month);
+      // Sunday = 0, Monday = 1...
+      this.monthStartsWith = this.getDayMonthStartsWith(year,month);
+      this.calendarRows = Math.ceil((this.daysInMonth+this.monthStartsWith)/7);
+      this.calendarArray = this.createCalendarArray();
     }
-    
+
     monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     dateObject = new Date();
     currentMonth: string = '';
     daysInMonth: number = 0;
     monthStartsWith: number = 0;
+    calendarRows: number = 0;
+    calendarArray: any;
 
     getDaysInMonth (year: number, month: number) {
         return new Date(year, month+1, 0).getDate();
@@ -26,5 +32,26 @@ export class StartComponent implements OnInit {
 
     getDayMonthStartsWith(year: number, month: number){
         return new Date(year, month, 1).getDay();
+    }
+
+    createCalendarArray() {
+      let totalCount = 0;
+      let daysLeft = this.daysInMonth;
+      let currentDay = 0;
+      let calendarArray = [];
+      for(let week = 0; week < this.calendarRows; week++) {
+        let weekArray = [];
+        for(let i=0; i<7; i++) {
+          if(totalCount >= this.monthStartsWith && daysLeft > 0) {
+            weekArray.push(++currentDay);
+            daysLeft--;
+          } else {
+            weekArray.push(-1);
+          }
+          totalCount++;
+        }
+        calendarArray.push(weekArray);
+      }
+      return calendarArray;
     }
 }
