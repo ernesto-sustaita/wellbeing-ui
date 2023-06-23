9b33a0f171b5721374235a598b6059100c8c3af0
ernesto-sustaita <50428180+ernesto-sustaita@users.noreply.github.com>
2023-06-22 18:26:46 -0600

    First version of calendar connected with DB

---
+++ wellbeing-ui/src/app/dto/calendarDay.out.ts
@@ -0,0 +1,4 @@
+export class CalendarDay {
+  dayNumber: string = '';
+  meditationSession: boolean = false;
+}
--- wellbeing-ui/src/app/services/activity.service.ts
+++ wellbeing-ui/src/app/services/activity.service.ts
@@ -5,19 +5,24 @@ import { Observable } from 'rxjs';
 
 @Injectable({providedIn:'root'})
 export class ActivityService {
- 
+
   baseURL: string = "https://localhost:7232/api/";
- 
+
   constructor(private http: HttpClient) {
   }
- 
+
   getActivities(): Observable<Activity[]> {
     console.log('getActivities '+this.baseURL + 'Activity')
     return this.http.get<Activity[]>(this.baseURL + 'Activity')
   }
- 
+
+  getActivitiesByDate(startDate: string, endDate: string): Observable<Activity[]> {
+    console.log('getActivities '+this.baseURL + 'Activity/' + startDate + '/' + endDate)
+    return this.http.get<Activity[]>(this.baseURL + 'Activity/' + startDate + '/' + endDate)
+  }
+
   addActivity(activity:Activity): Observable<any> {
-    const headers = { 'content-type': 'application/json'}  
+    const headers = { 'content-type': 'application/json'}
     const body=JSON.stringify(activity);
     console.log(body)
     return this.http.post(this.baseURL + 'Activity', body,{'headers':headers})
